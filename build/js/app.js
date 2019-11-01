6b842f024d002d000d44e9f7a4bcc494e79e8b76
Alla Panova <alla.mixalvna@gmail.com>
2019-11-01 21:33:56 +0300

    добавляет исправления после тестирования

--- build/js/app.js
+++ build/js/app.js
@@ -1,13 +1,24 @@
 'use strict';
-var pageNav = document.querySelector('.page-nav__list');
-var navToggle = document.querySelector('.page-nav__toggle');
+var hamburger = document.querySelector('.page-nav__toggle');
+var menu = document.querySelector('.page-nav__list');
 
-navToggle.addEventListener('click', function () {
-  if (pageNav.classList.contains('page-nav__list--closed')) {
-    pageNav.classList.remove('page-nav__list--closed');
-    pageNav.classList.add('page-nav__list--opened');
-  } else {
-    pageNav.classList.add('page-nav__list--closed');
-    pageNav.classList.remove('page-nav__list--opened');
+var toggleMenu = function toggleMenu() {
+  menu.classList.toggle('page-nav__list--active');
+};
+
+hamburger.addEventListener('click', function (e) {
+  e.stopPropagation();
+
+  toggleMenu();
+});
+
+document.addEventListener('click', function (e) {
+  var target = e.target;
+  var its_menu = target === menu || menu.contains(target);
+  var its_hamburger = target === hamburger;
+  var menu_is_active = menu.classList.contains('page-nav__list--active');
+
+  if (!its_menu && !its_hamburger && menu_is_active) {
+    toggleMenu();
   }
 });
